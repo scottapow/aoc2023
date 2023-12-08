bfd12c65946dc82267fef2bfa37046bf42ad4164
Scott Powell <spowell@adkgroup.com>
2023-12-07 21:06:06 -0500

    day 3: use engine struct

--- 3/main.go
+++ 3/main.go
@@ -8,21 +8,67 @@ import (
 	"strconv"
 )
 
-type Symbol struct {
+type symbol struct {
 	value string
 	index int
 }
-type Number struct {
+type number struct {
 	value      int
 	indexStart int
 	indexEnd   int
 }
+type engine struct {
+	symbols [][]symbol
+	numbers [][]number
+}
+
+func (e *engine) mapSymbols(line string) {
+	// map symbols
+	r := regexp.MustCompile(`(?m)&|%|\/|@|\+|\*|=|\$|#|-`)
+	matches := r.FindAllStringSubmatchIndex(line, -1)
+	if len(matches) != 0 {
+		var rowSymbols []symbol
+		for _, symbolIndex := range matches {
+			if len(symbolIndex) != 0 {
+				rowSymbols = append(rowSymbols, symbol{
+					value: string([]rune(line)[symbolIndex[0]]),
+					index: symbolIndex[0],
+				})
+			}
+		}
+		e.symbols = append(e.symbols, rowSymbols)
+	} else {
+		e.symbols = append(e.symbols, []symbol{})
+	}
+}
+func (e *engine) mapNumbers(line string) {
+	// map numbers
+	r := regexp.MustCompile(`(?s)\d+`)
+	matches := r.FindAllStringSubmatchIndex(line, -1)
+	if len(matches) != 0 {
+		var rowNumbers []number
+		for _, symbolIndex := range matches {
+			if len(symbolIndex) != 0 {
+				value, _ := strconv.Atoi(line[symbolIndex[0]:symbolIndex[1]])
+				rowNumbers = append(rowNumbers, number{
+					value:      value,
+					indexStart: symbolIndex[0],
+					indexEnd:   symbolIndex[1],
+				})
+			}
+		}
+		e.numbers = append(e.numbers, rowNumbers)
+	} else {
+		e.numbers = append(e.numbers, []number{})
+	}
+}
 
 func main() {
 	file, err := os.Open("data.txt")
-
-	var symbols [][]Symbol
-	var numbers [][]Number
+	e := &engine{
+		symbols: [][]symbol{},
+		numbers: [][]number{},
+	}
 
 	if err != nil {
 		panic(err)
@@ -30,48 +76,12 @@ func main() {
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		text := scanner.Text()
-
-		// map symbols
-		r := regexp.MustCompile(`(?m)&|%|\/|@|\+|\*|=|\$|#|-`)
-		matches := r.FindAllStringSubmatchIndex(text, -1)
-		if len(matches) != 0 {
-			var rowSymbols []Symbol
-			for _, symbolIndex := range matches {
-				if len(symbolIndex) != 0 {
-					rowSymbols = append(rowSymbols, Symbol{
-						value: string([]rune(text)[symbolIndex[0]]),
-						index: symbolIndex[0],
-					})
-				}
-			}
-			symbols = append(symbols, rowSymbols)
-		} else {
-			symbols = append(symbols, []Symbol{})
-		}
-
-		// map numbers
-		r = regexp.MustCompile(`(?s)\d+`)
-		matches = r.FindAllStringSubmatchIndex(text, -1)
-		if len(matches) != 0 {
-			var rowNumbers []Number
-			for _, symbolIndex := range matches {
-				if len(symbolIndex) != 0 {
-					value, _ := strconv.Atoi(text[symbolIndex[0]:symbolIndex[1]])
-					rowNumbers = append(rowNumbers, Number{
-						value:      value,
-						indexStart: symbolIndex[0],
-						indexEnd:   symbolIndex[1],
-					})
-				}
-			}
-			numbers = append(numbers, rowNumbers)
-		} else {
-			numbers = append(numbers, []Number{})
-		}
+		line := scanner.Text()
+		e.mapNumbers(line)
+		e.mapSymbols(line)
 	}
 
-	fmt.Print(numbers[0])
+	fmt.Print(e.numbers[0])
 
 	file.Close()
 }
